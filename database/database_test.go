7cd03ac45eb35b3a898707c96420cc457b203f09
TwinProduction <twin@twinnation.org>
2021-05-10 17:47:35 -0400

    Minor fix

--- database/database_test.go
+++ database/database_test.go
@@ -163,7 +163,7 @@ func TestGetOverdueRemindersRetrievesTheOldestOnesFirst(t *testing.T) {
 	}
 	for _, overdueReminder := range overdueReminders {
 		t.Logf("%+v\n", overdueReminder)
-		if overdueReminder.Time != now.Add(-3*time.Hour) && overdueReminder.Time != now.Add(-4*time.Hour) {
+		if overdueReminder.Time.After(now.Add(-2 * time.Hour)) {
 			t.Fatal("GetOverdueReminders should've only returned the 5 most overdue reminders")
 		}
 	}
