63a899e124674c8a3ed86ccf221af40b1639d7fa
TwinProduction <twin@twinnation.org>
2021-07-10 21:45:32 -0400

    Fix tests

--- core/reminder_test.go
+++ core/reminder_test.go
@@ -13,8 +13,8 @@ func TestReminder_GenerateNotificationMessageContent(t *testing.T) {
 		Note:                  "<Note>",
 		Time:                  time.Now().Add(time.Hour),
 	}
-	if reminder.GenerateNotificationMessageContent() != "I will remind you about <MessageLink> in 1h0m0s" {
-		t.Error("expected 'I will remind you about <MessageLink> in 1h0m0s', got", reminder.GenerateNotificationMessageContent())
+	if reminder.GenerateNotificationMessageContent() != "I will remind you about [this message](<MessageLink>) in 1h0m0s" {
+		t.Error("expected 'I will remind you about [this message](<MessageLink>) in 1h0m0s', got", reminder.GenerateNotificationMessageContent())
 	}
 }
 
@@ -26,7 +26,7 @@ func TestReminder_GenerateReminderMessageContent(t *testing.T) {
 		Note:                  "<Note>",
 		Time:                  time.Now().Add(time.Hour),
 	}
-	if reminder.GenerateReminderMessageContent() != "You asked me to remind you about <MessageLink> and attached the following note:\n```<Note>```" {
-		t.Error("expected 'You asked me to remind you about <MessageLink> and attached the following note:\n```<Note>```', got", reminder.GenerateReminderMessageContent())
+	if reminder.GenerateReminderMessageContent() != "You asked me to remind you about [this message](<MessageLink>) and attached the following note:\n```<Note>```" {
+		t.Error("expected 'You asked me to remind you about [this message](<MessageLink>) and attached the following note:\n```<Note>```', got", reminder.GenerateReminderMessageContent())
 	}
 }
